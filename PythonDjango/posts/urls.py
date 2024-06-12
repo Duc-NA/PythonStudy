c91d976f051840313f8339cd64052f5e2728bfcf
Nguyễn Anh Đức <ducna241099@gmail.com>
2024-06-12 09:59:05 +0700

    remove django

--- PythonDjango/posts/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("", views.get,name="get"),
-    path("new", views.post,name="post"),
-]
-               
-#djaneiro 
--- PythonDjango/users/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns = [
-    path("", views.get,name="get"),
-    path("<int:user_id>", views.getUserById,name="get"),
-]
-               
-#djaneiro 
